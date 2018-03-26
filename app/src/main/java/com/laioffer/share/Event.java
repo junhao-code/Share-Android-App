587c8e8eb3785fb9e9c66109b35ea0698201ae33
jus074 <jus074@eng.ucsd.edu>
2018-03-26 15:26:46 -0700

    event report activity added

--- app/src/main/java/com/laioffer/share/Event.java
+++ app/src/main/java/com/laioffer/share/Event.java
@@ -11,6 +11,15 @@ public class Event {
     private String title;
     private String address;
     private String description;
+    private int like;
+    private String id;
+    private long time;
+    private String username;
+    private String imgUri;
+
+
+    public Event() {}
+
 
     /**
      * Constructor
@@ -28,4 +37,35 @@ public class Event {
     public String getAddress() { return this.address; }
     public String getDescription() { return this.description; }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
 }
